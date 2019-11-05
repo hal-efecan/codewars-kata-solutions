8fd25bf917862d823c9d583ad0d920de2f3a588e
hal-efecan <hefecan@gmail.com>
2019-11-05 15:08:17 +0000

    Q13 add

--- 8kyu.js
+++ 8kyu.js
@@ -281,3 +281,39 @@ const sumMe = (num) => {
 sumMe(8)
 sumMe(2)
 */
+
+// TITLE: 13. Basic Mathematical Operations
+// DESCRIPTION: Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// EXAMPLE/QUESTION: 
+// basicOp('+', 4, 7)         // Output: 11
+// basicOp('-', 15, 18)       // Output: -3
+// basicOp('*', 5, 5)         // Output: 25
+// basicOp('/', 49, 7)        // Output: 7
+  
+// SOLUTION:
+
+const basicOp = (operation, value1, value2) => {
+    if(operation === '+') {
+        return value1 + value2
+    }
+    if(operation === '-') {
+        return value1 - value2
+    }
+    if(operation === '*') {
+        return value1 * value2
+    }
+    if(operation === '/') {
+        return value1 / value2
+    }
+    return 'Operation must be one of + - * /'
+}
+
+// TEST:
+
+console.log(basicOp('+', 4, 7)) //, 11);
+console.log(basicOp('-', 15, 18)) //, -3);
+console.log(basicOp('*', 5, 5)) //, 25);
+console.log(basicOp('/', 49, 7)) //, 7);
