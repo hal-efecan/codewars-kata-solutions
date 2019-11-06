a1dab5c5af8bc369c481f109ac8846242a8d9ebf
hal-efecan <hefecan@gmail.com>
2019-11-06 16:50:58 +0000

    Q18 8/7kyu commit

--- 8kyu.js
+++ 8kyu.js
@@ -377,7 +377,7 @@ TEST:
 numberToString(1)
 */
 
-// TITLE: 16. Square(n) Sum
+// TITLE: 17. Square(n) Sum
 
 // DESCRIPTION: Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
@@ -396,4 +396,51 @@ const squareSum = numbers => {
 }
 
 console.log(squareSum([1, 2, 2]))
+*/
+
+// TITLE: 18. Object Oriented Piracy
+
+// DESCRIPTION: Ahoy matey!
+// You are a leader of a small pirate crew. And you have a plan. With the help of OOP you wish to make a pretty
+// efficient system to identify ships with a heavy booty on board. Unfortunattely for you, people weigh a lot this days,
+// so how do you know if a ship if full of gold and not people? You begin with writing a generic Ship class:
+
+// EXAMPLE/QUESTION: 
+
+// function Ship(draft,crew) {
+//     this.draft = draft;
+//     this.crew = crew;
+//    }
+
+// Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+
+//     draft - an estimate of the ship's weight based on how low it is in the water
+//     crew - the count of crew on board
+
+// var titanic = new Ship(15, 10);
+
+// Taking into account that an average crew member on board adds 1.5 units to the draft, a ship that has a draft of more than 20 without crew is considered worthy to loot. Any ship weighing that much must have a lot of booty!
+
+// Add the method - isWorthIt
+// to decide if the ship is worthy to loot. For example:
+// titanic.isWorthIt() // return false
+
+// SOLUTION:
+/*
+class Ship {
+    constructor(draft, crew){
+        this.draft = draft,
+        this.crew = crew
+    }
+    isWorthIt(){
+        if(this.draft - (this.crew * 1.5) > 20){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+let Titanic = new Ship(0,0)
+
 */
