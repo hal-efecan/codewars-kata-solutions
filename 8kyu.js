1348d604ce5cb5365fc2816682ef4ab327f56e74
hal-efecan <hefecan@gmail.com>
2019-11-07 20:07:46 +0000

    Q21 8kyu commit

--- 8kyu.js
+++ 8kyu.js
@@ -528,3 +528,26 @@ console.log(ghost2.color)
 console.log(ghost3.color)
 console.log(ghost4.color)
 */
+
+// TITLE: 21. Basic subclasses - Adam and Eve
+
+// DESCRIPTION: According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
+// You have to do God's job. 
+// The creation method must return an array of length 2 containing objects (representing Adam and Eve). 
+// The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. 
+// Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
+
+// EXAMPLE/QUESTION:
+
+// SOLUTION:
+/*
+class God{
+    static create(){
+    return [new Man, new Woman];
+    }
+}
+
+class Human{}
+class Man extends Human{}
+class Woman extends Human{}
+*/
